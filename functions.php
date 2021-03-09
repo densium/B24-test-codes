d0c4d40b356aa17b1c162888e3e9f12a346db814
Denis Bryukhachev <75534743+densium@users.noreply.github.com>
2021-03-09 05:47:07 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,13 +1,27 @@
 <?
+
 AddEventHandler('tasks', 'OnTaskUpdate', 'TestHandler');
 
 function TestHandler($id, &$arFields) {
-	$url = 'https://telegram-client.it-solution.ru/pub_message/?chat_id=-541482224&message= ' .'ID:'.$id.' '. json_encode($arFields);
-   file_get_contents($url);
-   $arFields['DESCRIPTION'] .= ' Ругаться матом запрещено!';
-   if ($GLOBALS['USER']->GetID() == 2) {
-      $GLOBALS['APPLICATION']->throwException('Вы не можете создавать группы.');
-      return false;
-   }
+	//$res = $arFields["META:PREV_FIELDS"]["UF_CRM_TASK"][0]; // Сделка привязанная к задаче
+	$res = $arFields["META:PREV_FIELDS"]["DEPENDS_ON"][0]; // Связанная задача
+	// статус продумать
+
+	//id ($res === null)
+	//{return;}
+
+	$url = 'https://telegram-client.it-solution.ru/pub_message/?chat_id=-541482224&message= ' .'ID:'.$id.' '. $res;
+  	file_get_contents($url);
+
+	CModule::IncludeModule("bizproc");
+
+	$id = 5; // ID Сделки
+	$bizProcId = 28; // ID Шаблона бизнес-процесса
+	$arErrors = array();
+
+	CBPDocument::StartWorkflow($bizProcId, array('crm', 'DEAL', 'D_'.$id), 0, $arErrors);
+
 }
+
+
 ?>
